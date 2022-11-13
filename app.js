cb0d7d2cb7bd1a0e3631a729b6ec7aec12319ddb
NARESH CHOUDHARY <nareshchoudhary@NARESHs-MacBook-Pro.local>
2022-11-13 16:31:30 +0530

    added gitignore and Procfile and updated port

--- app.js
+++ app.js
@@ -133,6 +133,10 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
